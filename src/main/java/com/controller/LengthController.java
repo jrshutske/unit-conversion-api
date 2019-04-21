91642735198da778d8ac325d1f2abc6569d52989
CChapman03 <ciaran.j.chapman@gmail.com>
2019-04-21 14:13:49 -0500

    Improved form.  Fixed an error where Ajax requests returned undefined.

--- src/main/java/com/controller/LengthController.java
+++ src/main/java/com/controller/LengthController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @Controller
 @SpringBootApplication
 public class LengthController {
